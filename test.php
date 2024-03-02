d85a88e2680b31a9fe0a5e4cec7aec30c5a8b957
aymensellaouti <aymen.sellaouti@gmail.com>
2024-03-02 11:04:11 +0100

    seance 2 PHP POO et PDO

--- test.php
+++ test.php
@@ -2,8 +2,21 @@
 
 $tab = ['a', 1, "test", 23, 3.5];
 
+/**
+ * Cette fonction permet de récupérer un sous tableau en se basant
+ * sur son type 
+ * 
+ * @param array $array : tableau en entrée 
+ * @param string $type : le type des éléments à extraire
+ * 
+ * @return array le tableau résultant 
+ * @throws Exception
+ */
 function findArrayByType (array $array, string $type) {
     $myFonction = "is_$type";
+    if (!function_exists($myFonction)) {
+        throw new Exception('Fonction innexistante');
+    }
     $result = [];
     foreach ($array as $element):
         if($myFonction($element)) {
